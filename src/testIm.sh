bb96f23534fab349487dceff775128afc6971472
tm769613@ohio.edu <tm769613@ohio.edu>
2023-03-23 13:00:11 -0400

    deer tiny config

--- src/testIm.sh
+++ src/testIm.sh
@@ -1,3 +1,3 @@
 # python detect_ros.py --weights weights/yolov7.pt --conf 0.55 --img-size 640
 # python detect.py --weights yolov7-tiny.pt --conf 0.25 --img-size 640 --source ./video/leopard6mm21122022.mp4
-python3 detect_live.py --weights ./weights/deer.pt --conf 0.25 --img-size 1120 --source ./video/deer.mp4
+python3 detect_live.py --weights ./weights/deer-tiny.pt --conf 0.35 --img-size 1120 --source ./video/deer.mp4
